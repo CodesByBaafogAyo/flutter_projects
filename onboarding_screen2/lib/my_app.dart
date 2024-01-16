3dd6934462dbbe311b51d6d5b1c4a8de3998e3a3
Ibukun Ayomide-Baafog <ayomideracheal12@gmail.com>
2024-01-16 17:17:22 +0000

    initial commit

---
+++ onboarding_screen2/lib/my_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'onboarding_screen_2A.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Onboarding2A(),
+    );
+  }
+}
